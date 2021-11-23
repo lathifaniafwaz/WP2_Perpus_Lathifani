ed55cb1eb1b06c04edf02515992bad298e931905
lathifaniafwaz <lathifaniafwaz123@gmail.com>
2021-11-23 20:16:44 +0700

    Tugas Pertemuan 11

--- application/controllers/autentifikasi.php
+++ application/controllers/autentifikasi.php
@@ -24,8 +24,8 @@ class Autentifikasi extends CI_Controller
     {
         $this->session->unset_userdata('email');
         $this->session->unset_userdata('role_id');
-
-        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Anda telah Logout!!</div>');
+        $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Anda telah logout!!</div>');
+        redirect('autentifikasi');
     }
     public function blok()
     {
@@ -66,15 +66,21 @@ class Autentifikasi extends CI_Controller
         $this->load->view('templates/aute_footer');
     } else {
         $email = $this->input->post('email', true);
-        $data = ['nama' => htmlspecialchars($this->input->post('nama', true)),'email' => htmlspecialchars($email),'image' => 'default.jpg','password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),'role_id' => 2,'is_active' => 0,'tanggal_input' => time()];
+        $data = [
+            'nama' => htmlspecialchars($this->input->post('nama', true)),
+            'email' => htmlspecialchars($email),
+            'image' => 'default.jpg',
+            'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+            'role_id' => 2,
+            'is_active' => 0,'tanggal_input' => time()
+        ];
         $this->ModelUser->simpanData($data); //menggunakan model
         $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Selamat!! akun member anda sudah dibuat. Silahkan Aktivasi Akun anda</div>');redirect('autentifikasi');
     }
 }
 private function _login()
     {
-        $email = htmlspecialchars($this->input->post('email', 
-        true));
+        $email = htmlspecialchars($this->input->post('email', true));
         $password = $this->input->post('password', true);
         $user = $this->ModelUser->cekData(['email' => $email])->row_array();
         //jika usernya ada
